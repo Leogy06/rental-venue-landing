66f783bd9e6d68560cf90543510b757579397c43
Leogy06 <leogy_santiIIan850@outlook.com>
2026-07-24 20:32:30 +0800

    update gallery pictures

--- src/data/gallery.ts
+++ src/data/gallery.ts
@@ -1,13 +1,22 @@
 import { IGalleryImage } from "@/types";
 
 export const galleryImages: IGalleryImage[] = [
-    { src: "/images/gallery-1.svg", alt: "Elegant wedding reception setup" },
-    { src: "/images/gallery-2.svg", alt: "Grand Ballroom with chandeliers" },
-    { src: "/images/gallery-3.svg", alt: "Garden Terrace evening event" },
-    { src: "/images/gallery-4.svg", alt: "Beautiful floral centerpieces" },
-    { src: "/images/gallery-5.svg", alt: "Corporate gala dinner" },
-    { src: "/images/gallery-6.svg", alt: "Birthday celebration decor" },
-    { src: "/images/gallery-7.svg", alt: "Cocktail hour on the terrace" },
-    { src: "/images/gallery-8.svg", alt: "Dance floor and lighting" },
-    { src: "/images/gallery-9.svg", alt: "Intimate lounge rehearsal dinner" },
+  {
+    src: "/images/gallery-wedding.jpg",
+    alt: "Elegant wedding reception setup",
+  },
+  {
+    src: "/images/gallery-ballroom.jpg",
+    alt: "Grand Ballroom with chandeliers",
+  },
+  { src: "/images/gallery-terrace.jpg", alt: "Garden Terrace evening event" },
+  { src: "/images/gallery-floral.jpg", alt: "Beautiful floral centerpieces" },
+  { src: "/images/gallery-gala.png", alt: "Corporate gala dinner" },
+  { src: "/images/gallery-birthday.png", alt: "Birthday celebration decor" },
+  { src: "/images/gallery-cocktail.png", alt: "Cocktail hour on the terrace" },
+  { src: "/images/gallery-dancefloor.jpg", alt: "Dance floor and lighting" },
+  {
+    src: "/images/gallery-intimate.jpg",
+    alt: "Intimate lounge rehearsal dinner",
+  },
 ];
